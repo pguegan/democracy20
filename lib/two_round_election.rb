4076df96861040b9efc0edb83fc1b16042a1c89c
Philippe Guégan <philippe.guegan@gmail.com>
2017-05-17 10:09:35 +0200

    Préparation TD

--- lib/two_round_election.rb
+++ lib/two_round_election.rb
@@ -15,25 +15,15 @@ class TwoRoundElection < Election
   private
 
   def absolute_winner
-    candidates.find do |candidate|
-      candidate.votes > total_votes / 2.0
-    end
-  end
-
-  def total_votes
-    candidates.sum &:votes
+    # TODO
   end
 
   def eliminate_first_round_losers
-    first_round_winners = candidates.max_by 2, &:votes
-    first_round_losers = candidates - first_round_winners
-    @rankings.each do |ranking|
-      ranking.eliminate *first_round_losers
-    end
+    # TODO
   end
 
   def second_round_winner
-    candidates.max_by &:votes
+    # TODO
   end
 
 end
